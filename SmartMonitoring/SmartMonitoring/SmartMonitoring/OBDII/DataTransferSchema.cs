4ccf10bdfa8bbfc941abe23c4fe24fe9f9cbec7f
David Berbil <davidberbil@hotmail.com>
2017-10-06 15:03:53 +0200

    Cambios menores (using innecesarios eliminados)

--- SmartMonitoring/SmartMonitoring/SmartMonitoring/OBDII/DataTransferSchema.cs
+++ SmartMonitoring/SmartMonitoring/SmartMonitoring/OBDII/DataTransferSchema.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SmartMonitoring.OBDII
+﻿namespace SmartMonitoring.OBDII
 {
     public class DataTransferSchema
     {
--- SmartMonitoring/SmartMonitoring/SmartMonitoring/OBDII/Parameters.cs
+++ SmartMonitoring/SmartMonitoring/SmartMonitoring/OBDII/Parameters.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SmartMonitoring.OBDII
+﻿namespace SmartMonitoring.OBDII
 {
     public class Parameters
     {
@@ -122,8 +116,7 @@ namespace SmartMonitoring.OBDII
 
         };
 
-        //Funciones
-
+  
 
     }
 }
