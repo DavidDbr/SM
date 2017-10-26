46056a9ea5f21b43f7a002579f32364408e38e9b
David <dbr33@alumnos.unican.es>
2017-10-26 10:24:37 +0200

    Añadida visibilidad y creación de bbdd modularizada

--- SmartMonitoring/SmartMonitoring/SmartMonitoring.Android/SQLAndroid.cs
+++ SmartMonitoring/SmartMonitoring/SmartMonitoring.Android/SQLAndroid.cs
@@ -20,27 +20,31 @@ namespace SmartMonitoring.Droid
 {
     public class SQLAndroid : ISQLite
     {
-        SQLiteConnection connection;
-        static int cont = 0;
+        static SQLiteConnection connection = null;
+
         public SQLiteConnection GetConnection()
         {
-            var fileName = "SmartMonitoring.db3";
-            var documentsPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
-            var path = Path.Combine(documentsPath, fileName);
-
-            connection = new SQLiteConnection(path);
-            if (connection != null & cont==0)
+            if (connection == null)
             {
-                initBBDD();
-                cont++;
+                createConnection();
             }
             return connection;
         }
 
+        public void createConnection()
+        {
+            var fileName = "SmartMonitoring.db3";
+            var documentsPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
+            var path = Path.Combine(documentsPath, fileName);
+            connection = new SQLiteConnection(path);
+
+
+        }
         //Estos métodos hay que ver en que clase deberían ir
 
         public void initBBDD()
         {
+
             connection.CreateTable<RPMData>();
             connection.CreateTable<DTCData>();
             connection.CreateTable<EngineTemperatureData>();
@@ -131,6 +135,90 @@ namespace SmartMonitoring.Droid
             connection.CreateTable<ShortTermSecondaryOxygenSensorTrim2_4>();
             connection.CreateTable<TimingAdvance>();
             connection.CreateTable<WarmsUpsCodesCleared>();
+            int VisibilidadCreada = connection.ExecuteScalar<int>("SELECT COUNT(*) FROM sqlite_master WHERE type='table' AND name='Visibilidad'");
+            connection.CreateTable<Visibilidad>();
+            if (VisibilidadCreada == 0)
+            {
+
+                connection.Insert(new Visibilidad()
+                {
+                    absoluteBarometricPressureVisible = 0,
+                    absoluteEvapSystemVaporPressureVisible = 0,
+                    absoluteLoadValueVisible = 0,
+                    absoluteThrottlePositionBVisible = 0,
+                    absoluteThrottlePositionCVisible = 0,
+                    absoluteThrottlePositionDVisible = 0,
+                    absoluteThrottlePositionEVisible = 0,
+                    absoluteThrottlePositionFVisible = 0,
+                    actualEngine_PercentTorqueVisible = 0,
+                    ambientTemperatureVisible = 1,
+                    calculatedEngineLoadValueVisible = 0,
+                    catalystTemperatureB1S1Visible = 0,
+                    catalystTemperatureB1S2Visible = 0,
+                    catalystTemperatureB2S1Visible = 0,
+                    catalystTemperatureB2S2Visible = 0,
+                    commanddEvaporativePurgeVisible = 0,
+                    commandedEGRVisible = 0,
+                    commandedThrottleActuatorValueVisible = 0,
+                    controlModuleVoltageVisible = 0,
+                    distanceTraveledSinceCodesClearedVisible = 0,
+                    distanceTraveledWithMILoVisible = 0,
+                    driverDemandEngine_PercentTorqueVisible = 0,
+                    EGRErrorVisible = 0,
+                    emissionRequirementsToWhichVehicleIsDesignedVisible = 0,
+                    engineFuelRateValueVisible = 0,
+                    engineOilTemperatureVisible = 1,
+                    enginePercentTorqueData_PercentageEnginePoint1Visible = 0,
+                    enginePercentTorqueData_PercentageEnginePoint2Visible = 0,
+                    enginePercentTorqueData_PercentageEnginePoint3Visible = 0,
+                    enginePercentTorqueData_PercentageEnginePoint4Visible = 0,
+                    enginePercentTorqueData_PercentageIdleVisible = 0,
+                    engineReferenceTorqueVisible = 0,
+                    engineTemperatureVisible = 1,
+                    ethanolFuelPercentageVisible = 0,
+                    evapSystemVaporPressureVisible = 0,
+                    fuelAirCommandedEquivalenceRatioVisible = 0,
+                    fuelInjectionTimingVisible = 0,
+                    fuelPressureVisible = 0,
+                    fuelRailAbsolutePressureVisible = 0,
+                    fuelRailGaugePressureVisible = 0,
+                    fuelSystemStatus_System1Visible = 0,
+                    fuelSystemStatus_System2Visible = 0,
+                    fuelTankLevelVisible = 1,
+                    fuelTypeVisible = 0,
+                    hybridBateryPackRemainingLifeVisible = 0,
+                    intakeManifoldAbsolutePressureValueVisible = 0,
+                    intakeTemperatureVisible = 0,
+                    longTermFuelTrimB1Visible = 0,
+                    longTermFuelTrimB2Visible = 0,
+                    longTermSecondaryOxygenSensorTrim1_3_Visible = 0,
+
+                    longTermSecondaryOxygenSensorTrim2_4_Visible = 0,
+                    mAFAirFlowRateVisible = 0,
+                    maximunValueAirFlowRateFromMassAirFlowSensorVisible = 0,
+                    relativeAcceleratorPedalPositionVisible = 0,
+                    relativeThrottlePositionVisible = 0,
+                    rpmVisible = 1,
+                    RunTimeSinceEngineStartVisible = 0,
+                    shortTermFuelTrimB1Visible = 0,
+                    shortTermFuelTrimB2Visible = 0,
+                    shortTermSecondaryOxygenSensorTrim1_3_Visible = 0,
+                    shortTermSecondaryOxygenSensorTrim2_4_Visible = 0,
+                    speedVisible = 1,
+                    throttlePositionVisible = 1,
+                    timeEngineStartVisible = 0,
+                    timeRunWithMILOnVisible = 0,
+                    timeSinceTroubleCodesClearedVisible = 0,
+                    timingAdvanceVisible = 0,
+                    warmsUpsCodesClearedVisible = 0
+
+
+                });
+            }
+
+
+
         }
+
     }
 }
